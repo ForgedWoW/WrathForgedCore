3f411c3544871cd562a38dacbca1fd3518a4599f
Jim Burlison <JBurlison@Gmail.com>
2023-08-26 21:42:36 -0400

    Started basic Serializer/deserializer for reading and sending to client.

---
+++ WrathForged.Serialization/BinaryReaderExtentions.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) Forged WoW LLC <https://github.com/ForgedWoW/WrathForgedCore>
+// Licensed under GPL-3.0 license. See <https://github.com/ForgedWoW/WrathForgedCore/blob/master/LICENSE> for full information.
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrathForged.Serialization
+{
+    public static class BinaryReaderExtentions
+    {
+        public static T Read<T>(this BinaryReader reader, T obj)
+        {
+            return obj;
+        }
+
+        public static T Read<T>(this BinaryReader reader) where T : new()
+        {
+            var obj = new T();
+
+            if (!Attribute.IsDefined(typeof(T), typeof(ForgeSerializableAttribute)))
+                throw new InvalidOperationException($"{typeof(T).Name} is not marked with ForgeSerializableAttribute.");
+
+            var properties = typeof(T).GetProperties()
+                .Where(prop => Attribute.IsDefined(prop, typeof(SerializablePropertyAttribute)))
+                .OrderBy(prop => ((SerializablePropertyAttribute)prop.GetCustomAttribute(typeof(SerializablePropertyAttribute))).Index)
+                .ToList();
+
+            foreach (var prop in properties)
+            {
+                var attribute = (SerializablePropertyAttribute)prop.GetCustomAttribute(typeof(SerializablePropertyAttribute));
+
+                if (prop.PropertyType.IsArray)
+                {
+                    var length = reader.ReadInt32(); // Read array length
+                    var array = Array.CreateInstance(prop.PropertyType.GetElementType(), length);
+
+                    for (int i = 0; i < length; i++)
+                    {
+                        array.SetValue(ReadValue(reader, prop.PropertyType.GetElementType(), attribute.OverrideType), i);
+                    }
+
+                    prop.SetValue(obj, array);
+                }
+                else if (typeof(IList).IsAssignableFrom(prop.PropertyType) && prop.PropertyType.IsGenericType)
+                {
+                    var count = reader.ReadInt32(); // Read list count
+                    var listType = prop.PropertyType.GetGenericArguments()[0];
+                    var list = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(listType));
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        list.Add(ReadValue(reader, listType, attribute.OverrideType));
+                    }
+
+                    prop.SetValue(obj, list);
+                }
+                else if (Attribute.IsDefined(prop.PropertyType, typeof(ForgeSerializableAttribute)))
+                {
+                    var value = ReadMethodForType(prop.PropertyType).Invoke(reader, null);
+                    prop.SetValue(obj, value);
+                }
+                else
+                {
+                    prop.SetValue(obj, ReadValue(reader, prop.PropertyType, attribute.OverrideType));
+                }
+            }
+
+            return obj;
+        }
+
+        private static object ReadValue(BinaryReader reader, Type actualType, TypeCode overrideType)
+        {
+            var typeToUse = overrideType != TypeCode.Empty ? overrideType : Type.GetTypeCode(actualType);
+
+            switch (typeToUse)
+            {
+                case TypeCode.Boolean:
+                    return reader.ReadBoolean();
+
+                case TypeCode.Byte:
+                    return reader.ReadByte();
+                // ... handle other cases similarly ...
+                default:
+                    throw new NotSupportedException($"TypeCode {typeToUse} not supported.");
+            }
+        }
+
+        private static MethodInfo ReadMethodForType(Type type)
+        {
+            var method = typeof(BinaryReaderExtensions).GetMethod("Read", BindingFlags.Public | BindingFlags.Static);
+            return method.MakeGenericMethod(type);
+        }
+    }
+}
---
+++ WrathForged.Serialization/BinaryWriterExtentions.cs
@@ -0,0 +1,130 @@
+﻿// Copyright (c) Forged WoW LLC <https://github.com/ForgedWoW/WrathForgedCore>
+// Licensed under GPL-3.0 license. See <https://github.com/ForgedWoW/WrathForgedCore/blob/master/LICENSE> for full information.
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrathForged.Serialization
+{
+    public static class BinaryWriterExtentions
+    {
+        public static void Write<T>(this BinaryWriter writer, T obj)
+        {
+            // Ensure the object is decorated with ForgeSerializableAttribute
+            if (!Attribute.IsDefined(typeof(T), typeof(ForgeSerializableAttribute)))
+                throw new InvalidOperationException($"{typeof(T).Name} is not marked with ForgeSerializableAttribute.");
+
+            // Reflect and get properties with SerializablePropertyAttribute in order
+            var properties = typeof(T).GetProperties()
+                .Where(prop => Attribute.IsDefined(prop, typeof(SerializablePropertyAttribute)))
+                .OrderBy(prop => ((SerializablePropertyAttribute)prop.GetCustomAttribute(typeof(SerializablePropertyAttribute))).Index)
+                .ToList();
+
+            foreach (var prop in properties)
+            {
+                var attribute = (SerializablePropertyAttribute)prop.GetCustomAttribute(typeof(SerializablePropertyAttribute));
+                var value = prop.GetValue(obj);
+
+                if (value is Array arrayValue)
+                {
+                    writer.Write(arrayValue.Length);  // Write the array length
+                    var elementType = prop.PropertyType.GetElementType();
+                    foreach (var item in arrayValue)
+                    {
+                        WriteValue(writer, item, elementType, attribute.OverrideType);  // Write each array item
+                    }
+                }
+                else if (value is IList listValue && value.GetType().IsGenericType)
+                {
+                    writer.Write(listValue.Count);  // Write the list count
+                    var elementType = prop.PropertyType.GetGenericArguments()[0];
+                    foreach (var item in listValue)
+                    {
+                        WriteValue(writer, item, elementType, attribute.OverrideType);  // Write each list item
+                    }
+                }
+                else if (Attribute.IsDefined(prop.PropertyType, typeof(ForgeSerializableAttribute)))
+                {
+                    Write(writer, value);  // Recursively serialize this object
+                }
+                else
+                {
+                    WriteValue(writer, value, prop.PropertyType, attribute.OverrideType);  // Regular single value write
+                }
+            }
+        }
+
+        // Enhanced helper method to encapsulate the value writing logic:
+        private static void WriteValue(BinaryWriter writer, object value, Type actualType, TypeCode overrideType)
+        {
+            // If an override type is provided, use it. Otherwise, derive from the actual type.
+            var typeToUse = overrideType != TypeCode.Empty ? overrideType : Type.GetTypeCode(actualType);
+
+            switch (typeToUse)
+            {
+                case TypeCode.Boolean:
+                    writer.Write(Convert.ToBoolean(value));
+                    break;
+
+                case TypeCode.Byte:
+                    writer.Write(Convert.ToByte(value));
+                    break;
+
+                case TypeCode.Char:
+                    writer.Write(Convert.ToChar(value));
+                    break;
+
+                case TypeCode.Decimal:
+                    writer.Write(Convert.ToDecimal(value));
+                    break;
+
+                case TypeCode.Double:
+                    writer.Write(Convert.ToDouble(value));
+                    break;
+
+                case TypeCode.Int16:
+                    writer.Write(Convert.ToInt16(value));
+                    break;
+
+                case TypeCode.Int32:
+                    writer.Write(Convert.ToInt32(value));
+                    break;
+
+                case TypeCode.Int64:
+                    writer.Write(Convert.ToInt64(value));
+                    break;
+
+                case TypeCode.SByte:
+                    writer.Write(Convert.ToSByte(value));
+                    break;
+
+                case TypeCode.Single:
+                    writer.Write(Convert.ToSingle(value));
+                    break;
+
+                case TypeCode.String:
+                    writer.Write(value as string);
+                    break;
+
+                case TypeCode.UInt16:
+                    writer.Write(Convert.ToUInt16(value));
+                    break;
+
+                case TypeCode.UInt32:
+                    writer.Write(Convert.ToUInt32(value));
+                    break;
+
+                case TypeCode.UInt64:
+                    writer.Write(Convert.ToUInt64(value));
+                    break;
+
+                default:
+                    throw new NotSupportedException($"TypeCode {typeToUse} not supported.");
+            }
+        }
+    }
+}
