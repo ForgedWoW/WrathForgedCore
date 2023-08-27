64bf4c40de401e2e28326b180e0a0e9dd3d45a30
Jim Burlison <JBurlison@Gmail.com>
2023-08-27 16:15:06 -0400

    re-added serialization

---
+++ WrathForged.Serialization/DependancyInjection.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Forged WoW LLC <https://github.com/ForgedWoW/WrathForgedCore>
+// Licensed under GPL-3.0 license. See <https://github.com/ForgedWoW/WrathForgedCore/blob/master/LICENSE> for full information.
+using Autofac;
+using Microsoft.Extensions.Configuration;
+
+namespace WrathForged.Serialization
+{
+    public static class DependancyInjection
+    {
+        public static ContainerBuilder RegisterSerialization(this ContainerBuilder builder, IConfiguration configuration)
+        {
+            return builder;
+        }
+    }
+}
---
+++ WrathForged.Serialization/SerializablePropertyAttribute.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Forged WoW LLC <https://github.com/ForgedWoW/WrathForgedCore>
+// Licensed under GPL-3.0 license. See <https://github.com/ForgedWoW/WrathForgedCore/blob/master/LICENSE> for full information.
+namespace WrathForged.Serialization
+{
+    public class SerializablePropertyAttribute : Attribute
+    {
+        public SerializablePropertyAttribute(uint index, ForgedTypeCode overrideType = ForgedTypeCode.Empty)
+        {
+            Index = index;
+            OverrideType = overrideType;
+        }
+
+        public uint Index { get; }
+        public ForgedTypeCode OverrideType { get; }
+    }
+}
