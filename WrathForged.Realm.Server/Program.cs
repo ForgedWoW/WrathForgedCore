818eb00f16daf7c313cdc4d393d5279a7c114482
Jim Burlison <JBurlison@Gmail.com>
2023-09-12 18:54:27 -0400

    removed test code

--- WrathForged.Realm.Server/Program.cs
+++ WrathForged.Realm.Server/Program.cs
@@ -22,8 +22,7 @@ builder.RegisterDatabase(configuration, Log.Logger);
 
 var container = builder.Build();
 container.InitializeCommon();
-var dbc = container.Resolve<DBCDatabase>();
-var item = dbc.Items.Where(i => i.Id == 1);
+
 container.Resolve<WoWClientServer>().TCPServer.Start();
 
 Log.Logger.Information("Realm Server started.");
