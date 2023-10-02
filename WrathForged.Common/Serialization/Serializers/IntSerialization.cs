4201c78451c50ac62e9918e1d7bd913955bf570a
Jim Burlison <JBurlison@Gmail.com>
2023-10-02 16:09:15 -0400

    Fix build

--- WrathForged.Common/Serialization/Serializers/IntSerialization.cs
+++ WrathForged.Common/Serialization/Serializers/IntSerialization.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Forged WoW LLC <https://github.com/ForgedWoW/WrathForgedCore>
 // Licensed under GPL-3.0 license. See <https://github.com/ForgedWoW/WrathForgedCore/blob/master/LICENSE> for full information.
 using WrathForged.Common.Networking;
+using WrathForged.Models;
 using WrathForged.Serialization.Models;
 
 namespace WrathForged.Common.Serialization.Serializers
