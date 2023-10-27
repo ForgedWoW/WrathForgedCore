adfd46ed5d555beb344731ea3fff08c35381b3dc
Jim Burlison <JBurlison@Gmail.com>
2023-10-27 09:25:17 -0400

    When getting ILogger for serilog, It resolvesxz it in the context of the injecting class

--- SSDI/Builder/ActivationBuilder.cs
+++ SSDI/Builder/ActivationBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using Serilog;
 using SSDI.Parameters;
 using SSDI.Registration;
 
@@ -6,6 +7,7 @@ namespace SSDI.Builder;
 
 public class ActivationBuilder
 {
+    private static readonly Type _loggerType = typeof(ILogger);
     private readonly Dictionary<Type, List<CachedConstructor[]>> _constructors = new();
     private readonly Dictionary<Type, LifestyleType> _lifetimes = new();
     private readonly Dictionary<Type, HashSet<object>> _singletonInstances = new();
@@ -55,21 +57,21 @@ public class ActivationBuilder
 
     public T Locate<T>() => (T)Locate(typeof(T));
 
-    public object Locate(Type type) => Locate(type, Array.Empty<IDIParameter>());
+    public object Locate(Type type, Type? owner = null) => Locate(type, owner, Array.Empty<IDIParameter>());
 
-    public T LocateWithPositionalParams<T>(params object[] parameters) => (T)Locate(typeof(T), parameters.Select((p, i) => new PositionalParameter(i, p)).ToArray());
+    public T LocateWithPositionalParams<T>(params object[] parameters) => (T)Locate(typeof(T), null, parameters.Select((p, i) => new PositionalParameter(i, p)).ToArray());
 
-    public T LocateWithNamedParameters<T>(params (string name, object value)[] parameters) => (T)Locate(typeof(T), parameters.Select(p => new NamedParameter(p.name, p.value)).ToArray());
+    public T LocateWithNamedParameters<T>(params (string name, object value)[] parameters) => (T)Locate(typeof(T), null, parameters.Select(p => new NamedParameter(p.name, p.value)).ToArray());
 
-    public T LocateWithTypedParams<T>(params object[] parameters) => (T)Locate(typeof(T), parameters.Select(p => new TypedParameter(p)).ToArray());
+    public T LocateWithTypedParams<T>(params object[] parameters) => (T)Locate(typeof(T), null, parameters.Select(p => new TypedParameter(p)).ToArray());
 
-    public T Locate<T>(params IDIParameter[] parameters) => (T)Locate(typeof(T), parameters);
+    public T Locate<T>(params IDIParameter[] parameters) => (T)Locate(typeof(T), null, parameters);
 
-    public T Locate<T>(int position, object value) => (T)Locate(typeof(T), new PositionalParameter(position, value));
+    public T Locate<T>(int position, object value) => (T)Locate(typeof(T), null, new PositionalParameter(position, value));
 
-    public T LocateWithParams<T>(params IDIParameter[] parameters) => (T)Locate(typeof(T), parameters);
+    public T LocateWithParams<T>(params IDIParameter[] parameters) => (T)Locate(typeof(T), null, parameters);
 
-    public object Locate(Type type, params IDIParameter[] parameters)
+    public object Locate(Type type, Type? owner, params IDIParameter[] parameters)
     {
         var isEnumerable = type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IEnumerable<>);
         type = isEnumerable ? type.GetGenericArguments()[0] : type;
@@ -82,7 +84,7 @@ public class ActivationBuilder
 
                 foreach (var a in alias)
                 {
-                    if (LocateInternal(a, isEnumerable, parameters) is IList listOfObj)
+                    if (LocateInternal(a, isEnumerable, owner, parameters) is IList listOfObj)
                         foreach (var o in listOfObj)
                             _ = list.Add(o);
                 }
@@ -92,24 +94,26 @@ public class ActivationBuilder
             else
             {
                 foreach (var a in alias)
-                    return LocateInternal(a, isEnumerable, parameters);
+                    return LocateInternal(a, isEnumerable, owner, parameters);
 
                 throw new Exception($"Could not find a concrete type for interface {type.FullName}");
             }
         }
         else
         {
-            return LocateInternal(type, isEnumerable, parameters);
+            return LocateInternal(type, isEnumerable, owner, parameters);
         }
     }
 
-    private object LocateInternal(Type type, bool isEnumerable, params IDIParameter[] parameters)
+    private object LocateInternal(Type type, bool isEnumerable, Type? owner, params IDIParameter[] parameters)
     {
         if (_lifetimes.TryGetValue(type, out var lifestyleType) &&
             lifestyleType == LifestyleType.Singleton &&
             _singletonInstances.TryGetValue(type, out var instance))
         {
-            return isEnumerable ? instance : instance.First();
+            var retVal = isEnumerable ? instance : instance.First();
+
+            return type == _loggerType && owner != null ? ((ILogger)retVal).ForContext(owner) : retVal;
         }
 
         if (_constructors.TryGetValue(type, out var constructors))
@@ -218,7 +222,7 @@ public class ActivationBuilder
                 break;
             }
 
-            parameters[i] = Locate(p.ParameterType);
+            parameters[i] = Locate(p.ParameterType, type);
             i++;
         }
 
